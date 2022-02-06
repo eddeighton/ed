1ceaaa242dfc692fcdcf06f3a1b9dd14e93ee7c0
foobar <foobar@foobar.com>
2022-02-06 05:20:12 +0000

    Commit made from fedora linux

--- api/ed/ref.hpp
+++ api/ed/ref.hpp
@@ -9,6 +9,9 @@
 
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 
 #include <vector>
 #include <string>

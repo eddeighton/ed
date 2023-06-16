28b2bc4656dd06b49d92c0654754ead29876f1fc
Ed Deighton <eddeighton@hotmail.co.uk>
2023-06-16 17:39:07 +0100

    changed identifier to allow upper case for first character

--- src/file.cpp
+++ src/file.cpp
@@ -19,30 +19,23 @@
 namespace Ed
 {
 
-
 /////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 EdNode::EdNode( const EdNode& n, EdNode* _pParent )
-    :   m_statement( n.getStatement() ),
-        m_pParent( _pParent )
+    : m_statement( n.getStatement() )
+    , m_pParent( _pParent )
 {
-    for( PtrVector::const_iterator
-        i = n.m_children.begin(),
-        iEnd = n.m_children.end();
-        i!=iEnd; ++i )
+    for( auto i = n.m_children.begin(), iEnd = n.m_children.end(); i != iEnd; ++i )
     {
         m_children.push_back( new EdNode( **i, this ) );
     }
 }
 
 EdNode::EdNode( const Node& n, EdNode* _pParent )
-    :   m_statement( n.statement ),
-        m_pParent( _pParent )
+    : m_statement( n.statement )
+    , m_pParent( _pParent )
 {
-    for( Node::Vector::const_iterator
-        i = n.children.begin(),
-        iEnd = n.children.end();
-        i!=iEnd; ++i )
+    for( auto i = n.children.begin(), iEnd = n.children.end(); i != iEnd; ++i )
     {
         m_children.push_back( new EdNode( *i, this ) );
     }
@@ -61,30 +54,24 @@ bool EdNode::match( const std::regex& reg ) const
 void EdNode::toNode( Node& n ) const
 {
     n.statement = getStatement();
-    for( PtrVector::const_iterator
-        i = m_children.begin(),
-        iEnd = m_children.end();
-        i!=iEnd; ++i )
+    for( auto i = m_children.begin(), iEnd = m_children.end(); i != iEnd; ++i )
     {
         Ed::Node childNode;
         n.children.push_back( childNode );
-        (*i)->toNode( n.children.back() );
+        ( *i )->toNode( n.children.back() );
     }
 }
 
 void EdNode::toNode( Node& n, const std::regex& reg ) const
 {
     n.statement = getStatement();
-    for( PtrVector::const_iterator
-        i = m_children.begin(),
-        iEnd = m_children.end();
-        i!=iEnd; ++i )
+    for( auto i = m_children.begin(), iEnd = m_children.end(); i != iEnd; ++i )
     {
-        if( (*i)->match( reg ) )
+        if( ( *i )->match( reg ) )
         {
             Ed::Node childNode;
             n.children.push_back( childNode );
-            (*i)->toNode( n.children.back(), reg );
+            ( *i )->toNode( n.children.back(), reg );
         }
     }
 }
@@ -92,13 +79,11 @@ void EdNode::toNode( Node& n, const std::regex& reg ) const
 bool operator==( const EdNode& left, const EdNode& right )
 {
     bool bEqual = false;
-    if( left.getStatement() == right.getStatement() &&
-        left.getChildren().size() == right.getChildren().size() )
+    if( left.getStatement() == right.getStatement() && left.getChildren().size() == right.getChildren().size() )
     {
-        EdNode::PtrVector::const_iterator i = left.getChildren().begin(),
-            iEnd = left.getChildren().end(),
-            j = right.getChildren().begin();
-        for( ; i!=iEnd && **i==**j; ++i, ++j );
+        auto i = left.getChildren().begin(), iEnd = left.getChildren().end(), j = right.getChildren().begin();
+        for( ; i != iEnd && **i == **j; ++i, ++j )
+            ;
         bEqual = ( i == iEnd ) ? true : false;
     }
     return bEqual;
@@ -106,14 +91,11 @@ bool operator==( const EdNode& left, const EdNode& right )
 
 struct TagConvertVisitorIndex : boost::static_visitor< boost::optional< unsigned int > >
 {
-    boost::optional< unsigned int > operator()( unsigned int uiIndex ) const
-    {
-        return uiIndex;
-    }
-    template< class T >
+    boost::optional< unsigned int > operator()( unsigned int uiIndex ) const { return uiIndex; }
+    template < class T >
     boost::optional< unsigned int > operator()( const T& other ) const
     {
-        return boost::optional< unsigned int >();
+        return {};
     }
 };
 
@@ -121,10 +103,7 @@ void EdNode::expandShorthand()
 {
     bool bUsedIndexZero = false;
 
-    for( PtrVector::const_iterator
-        i = m_children.begin(),
-        iEnd = m_children.end();
-        i!=iEnd; ++i )
+    for( auto i = m_children.begin(), iEnd = m_children.end(); i != iEnd; ++i )
     {
         EdNode* pNode = *i;
 
@@ -133,22 +112,24 @@ void EdNode::expandShorthand()
             const unsigned int uiIndex = pNode->m_statement.tagList.get().getShorthandIndex();
             if( uiIndex != TagList::NO_INDEX )
             {
-                //we have a shorthand index...
+                // we have a shorthand index...
                 if( getStatement().shorthand )
                 {
-                    if( boost::optional< const Ed::ShorthandVariant& > shOpt = 
-                        getStatement().shorthand.get().get( uiIndex ) )
+                    if( boost::optional< const Ed::ShorthandVariant& > shOpt
+                        = getStatement().shorthand.get().get( uiIndex ) )
                     {
-                        ShorthandVariant newShorthandVar( shOpt.get() );//copy the variant
-                        propagate( newShorthandVar );//propagate it - ( this propagates all parts recursively )
+                        ShorthandVariant newShorthandVar( shOpt.get() ); // copy the variant
+                        propagate( newShorthandVar ); // propagate it - ( this propagates all parts recursively )
 
-                        Shorthand newShorthand; //create new shorthand with the variant
+                        Shorthand newShorthand; // create new shorthand with the variant
                         newShorthand.children.push_back( newShorthandVar );
-                        boost::optional< Shorthand > newShorthandOpt( newShorthand ); 
+                        boost::optional< Shorthand > newShorthandOpt( newShorthand );
 
-                        overrideAndExtend( pNode->m_statement.shorthand, newShorthandOpt ); //now override and extend the original child shorthand with it
+                        overrideAndExtend(
+                            pNode->m_statement.shorthand,
+                            newShorthandOpt ); // now override and extend the original child shorthand with it
 
-                        pNode->m_statement.shorthand = newShorthandOpt; //finally set the child shorthand
+                        pNode->m_statement.shorthand = newShorthandOpt; // finally set the child shorthand
 
                         if( uiIndex == 0U )
                             bUsedIndexZero = true;
@@ -168,15 +149,12 @@ void EdNode::removeTypes()
 {
     PtrVector temp;
     temp.swap( m_children );
-    for( PtrVector::const_iterator
-        i = temp.begin(),
-        iEnd = temp.end();
-        i!=iEnd; ++i )
+    for( auto i = temp.begin(), iEnd = temp.end(); i != iEnd; ++i )
     {
-        if( (*i)->m_statement.declarator.identifier )
+        if( ( *i )->m_statement.declarator.identifier )
         {
             m_children.push_back( *i );
-            (*i)->removeTypes();
+            ( *i )->removeTypes();
         }
     }
 }
@@ -185,28 +163,27 @@ void EdNode::removeTypes()
 /////////////////////////////////////////////////////////////////////////////
 boost::filesystem::path BasicFileSystem::load( const std::string& strRelativePath, std::string& strContents )
 {
-    const boost::filesystem::path filePath = 
-        boost::filesystem::edsCannonicalise(
-            boost::filesystem::absolute( strRelativePath ) );
-    
+    const boost::filesystem::path filePath
+        = boost::filesystem::edsCannonicalise( boost::filesystem::absolute( strRelativePath ) );
+
     VERIFY_RTE_MSG( boost::filesystem::exists( filePath ), "Could not locate file: " << filePath );
     std::string str;
     boost::filesystem::loadAsciiFile( filePath, strContents );
-    
+
     return filePath;
 }
 
 /////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
 File::File( FileSystem& filesystem, const std::string& strPath )
-    :   m_filesystem( filesystem )
+    : m_filesystem( filesystem )
 {
     std::string str;
     m_filePath = m_filesystem.load( strPath, str );
 
-    //parse the file
+    // parse the file
     std::ostringstream osError;
-    ParseResult result = parse( str, m_originals, osError );
+    ParseResult        result = parse( str, m_originals, osError );
     if( !result.first )
     {
         THROW_RTE( "Error during parsing: " << m_filePath << " : " << osError.str() );
@@ -216,26 +193,25 @@ File::File( FileSystem& filesystem, const std::string& strPath )
         THROW_RTE( "Parsing failed to process entire file: " << m_filePath << "\n" << osError.str() );
     }
 
-    for( Node::Vector::const_iterator i = m_originals.begin(), iEnd = m_originals.end();i!=iEnd; ++i )
+    for( auto i = m_originals.begin(), iEnd = m_originals.end(); i != iEnd; ++i )
         m_children.push_back( new EdNode( *i, this ) );
 
-    //process inheritance
+    // process inheritance
     File* pThisFile = this;
-    std::for_each( m_children.begin(), m_children.end(), 
-        [ pThisFile ]( EdNode* pNode ) { pThisFile->inheritNode( pNode ); } );
+    std::for_each(
+        m_children.begin(), m_children.end(), [ pThisFile ]( EdNode* pNode ) { pThisFile->inheritNode( pNode ); } );
 }
 
 void File::inheritNode( EdNode* pNode )
 {
     inheritStatement( pNode );
     File* pThisFile = this;
-    std::for_each( pNode->m_children.begin(), pNode->m_children.end(), 
-        [ pThisFile ]( EdNode* pNode ) { pThisFile->inheritNode( pNode ); } );
+    std::for_each( pNode->m_children.begin(), pNode->m_children.end(),
+                   [ pThisFile ]( EdNode* pNode ) { pThisFile->inheritNode( pNode ); } );
 }
 
-boost::optional< const EdNode& > File::lookupType( const EdNode* pNode,
-                    const TypeList::const_iterator iter,
-                    const TypeList::const_iterator iterEnd )
+boost::optional< const EdNode& >
+File::lookupType( const EdNode* pNode, const TypeList::const_iterator iter, const TypeList::const_iterator iterEnd )
 {
     VERIFY_RTE( pNode );
 
@@ -247,16 +223,13 @@ boost::optional< const EdNode& > File::lookupType( const EdNode* pNode,
     }
     else
     {
-        for( EdNode::PtrVector::const_iterator
-            i = pNode->m_children.begin(),
-            iEnd = pNode->m_children.end(); i!=iEnd; ++i )
+        for( auto i = pNode->m_children.begin(), iEnd = pNode->m_children.end(); i != iEnd; ++i )
         {
-            if( !(*i)->getStatement().declarator.identifier &&
-                !(*i)->getStatement().declarator.typeList.empty() )
+            if( !( *i )->getStatement().declarator.identifier && !( *i )->getStatement().declarator.typeList.empty() )
             {
-                if( (*i)->getStatement().declarator.typeList.back() == *iter )
+                if( ( *i )->getStatement().declarator.typeList.back() == *iter )
                 {
-                    if( ( result = lookupType( *i, iter+1, iterEnd ) ) )
+                    if( ( result = lookupType( *i, iter + 1, iterEnd ) ) )
                         break;
                 }
             }
@@ -267,8 +240,8 @@ boost::optional< const EdNode& > File::lookupType( const EdNode* pNode,
 
         if( !result )
         {
-            //attempt searching inherited file
-            PtrMap::const_iterator iFind = m_inheritedFiles.find( pNode );
+            // attempt searching inherited file
+            auto iFind = m_inheritedFiles.find( pNode );
             if( iFind != m_inheritedFiles.end() )
                 result = lookupType( iFind->second.get(), iter, iterEnd );
         }
@@ -283,28 +256,29 @@ void File::inheritStatement( EdNode* pNode )
     {
         if( pNode->m_statement.declarator.typeList.empty() )
         {
-            //do regular inheritance using directive if it exists
+            // do regular inheritance using directive if it exists
             if( boost::optional< const EdNode& > n = locateNodeToInherit( pNode ) )
                 overrideAndExtend( n.get(), *pNode );
         }
         else
         {
             FileRef::Vector files;
-            VERIFY_RTE_MSG( !pNode->m_statement.getFileRefs( files ), "Node cannot have both type and inheritance directive: " << pNode->m_statement );
-            //inherit using the type
+            VERIFY_RTE_MSG( !pNode->m_statement.getFileRefs( files ),
+                            "Node cannot have both type and inheritance directive: " << pNode->m_statement );
+            // inherit using the type
             VERIFY_RTE( pNode->getParent() );
-            TypeList& typeList = pNode->m_statement.declarator.typeList;
+            TypeList&                        typeList = pNode->m_statement.declarator.typeList;
             boost::optional< const EdNode& > n = lookupType( pNode->getParent(), typeList.begin(), typeList.end() );
             VERIFY_RTE_MSG( n, "Failed to lookup type for: " << pNode->m_statement );
             overrideAndExtend( n.get(), *pNode );
-            typeList.clear(); //strip the typelist
+            typeList.clear(); // strip the typelist
         }
     }
     else
     {
         if( !pNode->m_statement.declarator.typeList.empty() )
         {
-            //do regular inheritance using directive if it exists
+            // do regular inheritance using directive if it exists
             if( boost::optional< const EdNode& > n = locateNodeToInherit( pNode ) )
                 overrideAndExtend( n.get(), *pNode );
         }
@@ -317,57 +291,52 @@ void File::inheritStatement( EdNode* pNode )
 
 struct PathConstructorVisitor : boost::static_visitor< std::string >
 {
-    std::string operator()( const std::string& str ) const
-    {
-        return str;
-    }
+    std::string operator()( const std::string& str ) const { return str; }
     std::string operator()( FileActionType actionType ) const
     {
         std::string strResult;
         switch( actionType )
         {
-        case eFileUp:
-            strResult = "..";
-            break;
-        case TOTAL_FILEREF_ACTION_TYPES:
-        default:
-            THROW_RTE( "Invalid file action type" );
-            break;
-
+            case eFileUp:
+                strResult = "..";
+                break;
+            case TOTAL_FILEREF_ACTION_TYPES:
+            default:
+                THROW_RTE( "Invalid file action type" );
+                break;
         }
         return strResult;
     }
 };
 
-void getFileRefPath( const FileRef& fileRef,
-    const boost::filesystem::path& current,
-    boost::filesystem::path& result )
+void getFileRefPath( const FileRef& fileRef, const boost::filesystem::path& current, boost::filesystem::path& result )
 {
     result = current;
     result.remove_filename();
-    for( FileRef::const_iterator i = fileRef.begin(),
-        iEnd = fileRef.end(); i!=iEnd; ++i )
+    for( auto i = fileRef.begin(), iEnd = fileRef.end(); i != iEnd; ++i )
     {
         std::string s = boost::apply_visitor( PathConstructorVisitor(), *i );
         result.append( s.begin(), s.end() );
     }
-    result = boost::filesystem::change_extension( result, ".ed" );
+    result = result.replace_extension( ".ed" );
 }
 
 struct LocateRefFromRootVisitor : boost::static_visitor< boost::optional< const EdNode& > >
 {
     const EdNode::PtrVector& nodes;
-    LocateRefFromRootVisitor( const EdNode::PtrVector& _nodes ) : nodes( _nodes ) {}
+    LocateRefFromRootVisitor( const EdNode::PtrVector& _nodes )
+        : nodes( _nodes )
+    {
+    }
 
     boost::optional< const EdNode& > operator()( const Identifier& identifier ) const
     {
         boost::optional< const EdNode& > result;
-        //return the first match
-        for( EdNode::PtrVector::const_iterator
-            i = nodes.begin(), iEnd = nodes.end(); i!=iEnd; ++i )
+        // return the first match
+        for( auto i = nodes.begin(), iEnd = nodes.end(); i != iEnd; ++i )
         {
-            if( (*i)->getStatement().declarator.identifier &&
-                (*i)->getStatement().declarator.identifier.get() == identifier )
+            if( ( *i )->getStatement().declarator.identifier
+                && ( *i )->getStatement().declarator.identifier.get() == identifier )
             {
                 result = **i;
                 break;
@@ -379,18 +348,18 @@ struct LocateRefFromRootVisitor : boost::static_visitor< boost::optional< const 
     boost::optional< const EdNode& > operator()( const RefActionType& type ) const
     {
         THROW_RTE( "Invalid use of ref . in inheritance directive" );
-        return boost::optional< const EdNode& >();
+        return {};
     }
 
     boost::optional< const EdNode& > operator()( const Ref& type ) const
     {
         THROW_RTE( "Invalid use of ref . in inheritance directive" );
-        return boost::optional< const EdNode& >();
+        return {};
     }
 };
 
-boost::optional< const EdNode& > getRefNode( const EdNode::PtrVector& nodes,
-        Reference::const_iterator i, Reference::const_iterator iEnd )
+boost::optional< const EdNode& > getRefNode( const EdNode::PtrVector& nodes, Reference::const_iterator i,
+                                             Reference::const_iterator iEnd )
 {
     boost::optional< const EdNode& > result;
     if( i != iEnd )
@@ -414,11 +383,11 @@ boost::optional< const EdNode& > File::locateNodeToInherit( EdNode* pNode )
     FileRef::Vector files;
     if( pNode->m_statement.getFileRefs( files ) )
     {
-        const FileRef& inheritedFile = files.back();
+        const FileRef&          inheritedFile = files.back();
         boost::filesystem::path targetFile;
         getFileRefPath( files.back(), m_filePath, targetFile );
 
-        //load the file and recursively process it
+        // load the file and recursively process it
         File::Ptr pFile( new File( m_filesystem, targetFile.string() ) );
         m_inheritedFiles.insert( std::make_pair( pNode, pFile ) );
         Ref::Vector refs;
@@ -431,7 +400,7 @@ boost::optional< const EdNode& > File::locateNodeToInherit( EdNode* pNode )
         }
         else
         {
-            nodeToInherit = *(pFile.get());
+            nodeToInherit = *( pFile.get() );
         }
     }
 
@@ -442,9 +411,8 @@ boost::optional< const EdNode& > File::locateNodeToInherit( EdNode* pNode )
 /////////////////////////////////////////////////////////////////////////////
 std::string saveNodeToFile( const std::string& strFile, const Node& node )
 {
-    const boost::filesystem::path p = boost::filesystem::edsCannonicalise(
-            boost::filesystem::absolute( strFile ) );
-    const std::string strActualFilePath = p.generic_string().c_str();
+    const boost::filesystem::path p = boost::filesystem::edsCannonicalise( boost::filesystem::absolute( strFile ) );
+    const std::string             strActualFilePath = p.generic_string().c_str();
 
     std::ofstream of( strActualFilePath.c_str(), std::ofstream::out | std::ofstream::trunc );
     if( !of.good() )
@@ -452,8 +420,7 @@ std::string saveNodeToFile( const std::string& strFile, const Node& node )
         THROW_RTE( "Failed to create file: " << strActualFilePath );
     }
 
-    for( Node::Vector::const_iterator i = node.children.begin(),
-        iEnd = node.children.end(); i!=iEnd; ++i )
+    for( auto i = node.children.begin(), iEnd = node.children.end(); i != iEnd; ++i )
         of << *i;
 
     return strActualFilePath;
@@ -467,4 +434,4 @@ bool operator==( const File& left, const File& right )
     return n1 == n2;
 }
 
-}
+} // namespace Ed
